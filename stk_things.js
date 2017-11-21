e7d484c2470a5a56bb7c4ebe30db8aa256cc1cbb
David Becerril <david@3db.in>
2017-11-21 16:22:12 +0100

    Updated for stockholm things ep

---
+++ stk_things.js
@@ -0,0 +1,32 @@
+
+var s = Snap("#stk_t");
+
+var l = Snap.load("stk_things.svg", onSVGLoaded);
+
+function onSVGLoaded(data) {
+  s.append(data);
+  main()
+}
+
+
+function f_in() {
+  console.log("hovering");
+  var scalef = 1.5;
+  var c = this.getBBox();
+  cx = (c.x+c.x+c.width)/2;
+  cy = (c.y+c.y+c.width)/2;
+  var xtr = (1 - scalef)*(cx);
+  var ytr = (1 - scalef)*(cy);
+  this.animate({transform: 'translate(' + xtr + ', ' + ytr + ') scale(' + scalef + ') '}, 400, mina.elastic);
+}
+function f_out() {
+  console.log("hovering");
+  this.animate({transform: 'translate(0,0) scale(1)'}, 400, mina.elastic);
+}
+
+function main() {
+  //var cr_w = s.select("#cr_w");
+  var cr_w = s.select("g#gcenter");
+
+  cr_w.hover(f_in, f_out);
+}
